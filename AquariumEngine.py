2e8bf1656deb5a6ce6ca2d5ec19aa9200e2d79ae
AllegraNegri <99329717+AllegraNegri@users.noreply.github.com>
2022-03-07 11:54:58 +0000

    answered ten questions from first specification

--- AquariumEngine.py
+++ AquariumEngine.py
@@ -1,20 +1,28 @@
 # import all of the necessary libraries
+import random
 import sys, pygame
 import os
 from pygame.locals import *
 import AquariumGraphics
+
+game_side_padding = 25
+
 
 class Food():
     def __init__(self, x_pos, y_pos):
         self.x = x_pos
         self.y = y_pos
         self.radius = 15
+        self.width = 2 * self.radius
+        self.height = self.width
+        self.speed = random.randint(1, 30)
 
     def move_down(self):
         ######################################################
         # Q7: Fill in this method
         ######################################################
-        pass
+        speed = random.randint(1, 5)
+        self.y += speed
 
 
 class Pipe():
@@ -26,15 +34,28 @@ class Pipe():
         self.food_pieces = []
 
     def add_food(self):
-        self.food_pieces.append(Food(self.x, 70))
+        addingfood = random.randint(1, 100)
+        if addingfood == 1 or 2:
+            self.food_pieces.append(Food(self.x, 49))
 
-    def move_food(self, boundary_y):
+    def move_food(self, boundary_y, player):
         ######################################################
         # Q9: Remove food from self.food_pieces if it falls outside the game
         ######################################################
-
+        counter = 0
         for food in self.food_pieces:
+            if food.y > boundary_y:
+                self.food_pieces.remove(food)
             food.move_down()
+
+            if food.x < player.width + player.x \
+                    and food.width + food.x > player.x \
+                    and food.y + food.height > player.y \
+                    and food.y < player.y + player.height:
+                self.food_pieces.remove(food)
+                counter += 1
+
+        return counter
 
 
 class Fish(pygame.sprite.Sprite):
@@ -43,32 +64,41 @@ class Fish(pygame.sprite.Sprite):
         self.x = x_pos
         self.y = y_pos
         self.aquarium_width = aquarium_width
-        self.speed = 5
+        self.speed = 15
 
         ######################################################
         # Q1: Set the height and width of the player to suitable values
         ######################################################
-        self.height = 200
-        self.width = 200
+        self.height = 56
+        self.width = 70
 
         # Load image for player
         # (you can open the image in the assets folder to see what it looks like)
-        image_unscaled = pygame.image.load(os.path.join("assets", "goldfish_left.png"))
+        image_unscaledleft = pygame.image.load(os.path.join("assets", "goldfish_left.png"))
+        image_unscaledright = pygame.image.load(os.path.join("assets", "goldfish_right.png"))
 
         # Scale the player's character have the specified height and width
-        self.image = pygame.transform.rotate(pygame.transform.scale(
-            image_unscaled, (self.width, self.height)), 0)
+        self.image_left = pygame.transform.rotate(pygame.transform.scale(
+            image_unscaledleft, (self.width, self.height)), 0)
 
-        self.rect = self.image.get_rect()
+        self.rect = self.image_left.get_rect()
+        self.image = self.image_left
+
+        self.image_right = pygame.transform.rotate(pygame.transform.scale(
+            image_unscaledright, (self.width, self.height)), 0)
+
+        self.rect = self.image_right.get_rect()
 
     def handle_movement(self, keys_pressed):
         """
         Update player's position according to the key pressed
         """
-        if keys_pressed[pygame.K_LEFT]:
+        if keys_pressed[pygame.K_LEFT] and self.x >= 30:
             self.x -= self.speed
-        elif keys_pressed[pygame.K_RIGHT]:
-            self.x -= self.speed
+            self.image = self.image_left
+        elif keys_pressed[pygame.K_RIGHT] and self.x <= 820:
+            self.x += self.speed
+            self.image = self.image_right
         else:
             return
         self.rect.x = self.x
@@ -82,14 +112,14 @@ class Aquarium():
 
         self.score = 0
         self.game_running = True
-
+        self.prob_food=0.05
         ## game constants
         self.width = width
         self.height = height
 
         ## player (the fish)
-        self.player = Fish(x_pos=450,
-                           y_pos=290,
+        self.player = Fish(x_pos=width / 2,
+                           y_pos=height * 9 / 10,
                            aquarium_width=self.width)
 
         ## pipe
@@ -115,6 +145,10 @@ class Aquarium():
             # Add food to the pipe
             # self.pipe.add_food()
 
+            addingfood = random.randint(1, 100)
+            if addingfood/100 < self.prob_food:
+                self.pipe.add_food()
+
             # Process all events
             for event in pygame.event.get():
                 if event.type == QUIT:
@@ -126,7 +160,8 @@ class Aquarium():
             self.player.handle_movement(keys_pressed)
 
             # Move food down pipe
-            self.pipe.move_food(boundary_y=self.height)
+            counter = self.pipe.move_food(boundary_y=self.height + AquariumGraphics.top_offset, player=self.player)
+            self.score += counter
 
             # Refresh the display and loop back
             self.draw()
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     # Initialise the aquarium game engine
     # Modify the parameters to adjust the game setup
     app = AquariumEngine.Aquarium(
-        width=580,
+        width=880,
         height=520
     )
     app.game_loop()
